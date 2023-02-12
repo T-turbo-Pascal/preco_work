345a03b28ccf078938528d6f6da65b22689108fe
T-turbo-Pascal <richard00111010@gmail.com>
2023-02-12 10:41:11 +0100

    sidebar has been redesigned to be more beautiful

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
-import { NavigationContainer, DrawerActions } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Drawer, Divider, Avatar, Title, Caption } from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import HomePage from './pages/HomePage';
 import TodoPage from './pages/TodoPage';
@@ -11,125 +12,138 @@ import EventsPage from './pages/EventsPage';
 import SettingsPage from './pages/SettingsPage';
 import AboutUsPage from './pages/AboutUsPage';
 
-const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
+const DrawerNavigator = createDrawerNavigator();
 
 function CustomDrawerContent({ navigation }) {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={{ flex: 1, backgroundColor: '#fff' }}>
-        <View style={styles.headerContainer}>
-          <Image
-            source={require('./assets/img/logo_university.jpg')}
-            style={styles.profileImage}
-          />
-          <View style={styles.headerTextContainer}>
-            <Text style={styles.headerText}>Faculty of Management</Text>
-            <Text style={styles.headerText}>
-              Comenius University in Bratislava
-            </Text>
-          </View>
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <View style={styles.headerContainer}>
+        <Avatar.Image
+          source={require('./assets/img/logo_university.jpg')}
+          size={80}
+        />
+        <View style={styles.headerTextContainer}>
+          <Title style={styles.headerText}>Faculty of Management</Title>
+          <Caption style={styles.headerText}>
+            Comenius University in
+          </Caption>
+          <Caption style={styles.headerText}>
+            Bratislava
+          </Caption>
         </View>
-        <TouchableOpacity
-          style={{ margin: 16 }}
-          onPress={() => {
-            navigation.navigate('Home');
-            navigation.dispatch(DrawerActions.closeDrawer());
-          }}>
-          <Text>Home</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{ margin: 16 }}
-          onPress={() => {
-            navigation.navigate('Todo');
-            navigation.dispatch(DrawerActions.closeDrawer());
-          }}>
-          <Text>Todo</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={{ margin: 16 }}
-          onPress={() => {
-            navigation.navigate('Aktuality');
-            navigation.dispatch(DrawerActions.closeDrawer());
-          }}>
-          <Text>Aktuality</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={{ margin: 16 }}
-          onPress={() => {
-            navigation.navigate('Podujatia');
-            navigation.dispatch(DrawerActions.closeDrawer());
-          }}>
-          <Text>Podujatia</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{ margin: 16 }}
-          onPress={() => {
-            navigation.navigate('Nastavenia');
-            navigation.dispatch(DrawerActions.closeDrawer());
-          }}>
-          <Text>Nastavenia</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{ margin: 16 }}
-          onPress={() => {
-            navigation.navigate('O nás');
-            navigation.dispatch(DrawerActions.closeDrawer());
-          }}>
-          <Text>O nás</Text>
-        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+      <Drawer.Item
+        label="Home"
+        onPress={() => {
+          navigation.navigate('Home');
+        }}
+        icon={({ color, size }) => <MaterialCommunityIcons name="home" color={color} size={size} />}
+      />
+      <Drawer.Item
+        label="Todo"
+        onPress={() => {
+          navigation.navigate('Todo');
+        }}
+        icon={({ color, size }) => <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />}
+      />
+      <Drawer.Item
+        label="Aktuality"
+        onPress={() => {
+          navigation.navigate('Aktuality');
+        }}
+        icon={({ color, size }) => <MaterialCommunityIcons name="newspaper" color={color} size={size} />}
+      />
+      <Drawer.Item
+        label="Podujatia"
+        onPress={() => {
+          navigation.navigate('Podujatia');
+        }}
+        icon={({ color, size }) => <MaterialCommunityIcons name="calendar" color={color} size={size} />}
+      />
+      <Drawer.Item
+        label="Nastavenia"
+        onPress={() => {
+          navigation.navigate('Nastavenia');
+        }}
+        icon={({ color, size }) => <MaterialCommunityIcons name="cog" color={color} size={size} />}
+      />
+      <Drawer.Item
+        label="O nás"
+        onPress={() => {
+          navigation.navigate('O nás');
+        }}
+        icon={({ color, size }) => <MaterialCommunityIcons name="information" color={color} size={size} />}
+      />
+      <Divider />
+      <Drawer.Item
+        label="Odhlásiť sa"
+        onPress={() => {
+          // Code to handle logout action
+        }}
+        icon={({ color, size }) => <MaterialCommunityIcons name="logout" color={color} size={size} />}
+      />
+    </View>
   );
 }
 
 function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
-        <Drawer.Screen name="Home" component={HomePage} />
-        <Drawer.Screen name="Todo" component={TodoPage} />
-        <Drawer.Screen name="Aktuality" component={NewsFeedPage} />
-        <Drawer.Screen name="Podujatia" component={EventsPage} />
-        <Drawer.Screen name="Nastavenia" component={SettingsPage} />
-        <Drawer.Screen name="O nás" component={AboutUsPage} />
-      </Drawer.Navigator>
-      
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <DrawerNavigator.Navigator
+          initialRouteName="Home"
+          drawerContent={props => <CustomDrawerContent {...props} />}
+        >
+          <DrawerNavigator.Screen
+            name="Home"
+            component={HomePage}
+            options={{ title: 'Domov' }}
+          />
+          <DrawerNavigator.Screen
+            name="Todo"
+            component={TodoPage}
+            options={{ title: 'Úlohy' }}
+          />
+          <DrawerNavigator.Screen
+            name="Aktuality"
+            component={NewsFeedPage}
+            options={{ title: 'Aktuality' }}
+          />
+          <DrawerNavigator.Screen
+            name="Podujatia"
+            component={EventsPage}
+            options={{ title: 'Podujatia' }}
+          />
+          <DrawerNavigator.Screen
+            name="Nastavenia"
+            component={SettingsPage}
+            options={{ title: 'Nastavenia' }}
+          />
+          <DrawerNavigator.Screen
+            name="O nás"
+            component={AboutUsPage}
+            options={{ title: 'O nás' }}
+          />
+        </DrawerNavigator.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
-
 const styles = StyleSheet.create({
-  safeArea: {
-    paddingTop: 20,
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
   headerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
     padding: 20,
-  },
-  profileImage: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   headerTextContainer: {
-    marginLeft: 10,
+    marginLeft: 20,
+    paddingTop: 10,
   },
   headerText: {
-    fontSize: 16,
     fontWeight: 'bold',
+    color: '#333'
   },
 });
 
 export default App;
-
-
